47c607390d4ab2e431bc86a1ac3910c5219e472b
surajtripathi1 <suraj@locus.sh>
2015-11-10 00:46:50 +0530

    make it better3 :)

--- js/mainCube.js
+++ js/mainCube.js
@@ -131,8 +131,9 @@ var rotateZ3D = function(theta) {
 initCanvas = function() {
     canvas = document.getElementById('cube');
     if (screen.width < 600 ) {
-        document.getElementById('container').style.width = window.innerHeight + "px";
-        canvas_width = window.innerHeight;
+        var ratio = window.devicePixelRatio || 1;
+        document.getElementById('container').style.width = screen.width + "px";
+        canvas_width = screen.width * ratio;
         sideLen = 0.7 * canvas_width;
     } else {
         document.getElementById('container').style.width = '620px';
